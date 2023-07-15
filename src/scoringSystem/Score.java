a43ad3a925954106ee1032d909c84446ed90c23f
Vinodkumar M <vinod.kumar@improve10x.com>
2023-07-16 02:39:54 +0530

    Changes did in ScoringSystem.

--- src/scoringSystem/Score.java
+++ src/scoringSystem/Score.java
@@ -2,15 +2,17 @@ package scoringSystem;
 
 public class Score {
     public int[] calculateScores(String score) {
-        int [] scores = new int[3];
-        for (int i = 0; i<score.length(); i++){
-            char c = score.charAt(i);
-            if(c == 'A'){
-                scores[0]++;
-            } else if(c == 'B'){
-                scores[1]++;
-            } else if(c == 'C'){
-                scores[2]++;
+        int[] scores = new int[3];
+        if (score != null) {
+            for (int i = 0; i < score.length(); i++) {
+                char c = score.charAt(i);
+                if (c == 'A') {
+                    scores[0]++;
+                } else if (c == 'B') {
+                    scores[1]++;
+                } else if (c == 'C') {
+                    scores[2]++;
+                }
             }
         }
         return scores;
--- src/scoringSystem/Scoring.java
+++ src/scoringSystem/Scoring.java
@@ -1,77 +1,86 @@
 package scoringSystem;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.testng.annotations.Test;
 
 import static org.testng.internal.junit.ArrayAsserts.assertArrayEquals;
 
 public class Scoring {
+    @BeforeEach
+    public void setup() {
+    }
     @Test
     public void nothing(){
     }
-
     @Test
-    public void givenEmptyReturnsAllZero() {
+    public void givenNull_ReturnsAllZero() {
+        Score score = new Score();
+        int[] allZeros = score.calculateScores(null);
+        assertArrayEquals(new int[3], allZeros);
+    }
+    @Test
+    public void givenEmpty_ReturnsAllZero() {
         Score score = new Score();
         int[] allZeros = score.calculateScores("");
         assertArrayEquals(new int[3], allZeros);
     }
 
     @Test
-    public void givenAReturnsOneZeroZero() {
+    public void givenA_ReturnsOneZeroZero() {
         Score score = new Score();
         int[] onlyAScores = score.calculateScores("A");
         assertArrayEquals(new int[]{1, 0, 0}, onlyAScores);
     }
     @Test
-    public void givenBReturnsZeroOneZero() {
+    public void givenB_ReturnsZeroOneZero() {
         Score score = new Score();
         int[] onlyBScores = score.calculateScores("B");
         assertArrayEquals(new int[]{0, 1, 0}, onlyBScores);
     }
 
     @Test
-    public void givenCReturnsZeroZeroOne() {
+    public void givenC_ReturnsZeroZeroOne() {
         Score score = new Score();
         int[] onlyCScore = score.calculateScores("C");
         assertArrayEquals(new int[]{0, 0, 1}, onlyCScore);
     }
 
     @Test
-    public void givenABReturnsOneOneZero() {
+    public void givenAB_ReturnsOneOneZero() {
         Score score = new Score();
         int[] abScore = score.calculateScores("AB");
         assertArrayEquals(new int[]{1, 1, 0}, abScore);
     }
     @Test
-    public void givenABCReturnsAllOnes() {
+    public void givenABC_ReturnsAllOnes() {
         Score score = new Score();
         int[] abcScore = score.calculateScores("ABC");
         assertArrayEquals(new int[]{1, 1, 1}, abcScore);
     }
 
     @Test
-    public void givenAABCReturnsTwoOneOne() {
+    public void givenAABC_ReturnsTwoOneOne() {
         Score score = new Score();
         int[] aabcScore = score.calculateScores("AABC");
         assertArrayEquals(new int[]{2, 1, 1}, aabcScore);
     }
 
     @Test
-    public void givenABBCReturnsOneTwoOne() {
+    public void givenABBC_ReturnsOneTwoOne() {
         Score score = new Score();
         int[] abbc = score.calculateScores("ABBC");
         assertArrayEquals(new int[]{1, 2, 1}, abbc);
     }
 
     @Test
-    public void givenABCCReturnsOneOneTwo() {
+    public void givenABCC_ReturnsOneOneTwo() {
         Score score = new Score();
         int[] abcc = score.calculateScores("ABCC");
         assertArrayEquals(new int[]{1, 1, 2}, abcc);
     }
 
     @Test
-    public void givenABBACCCCACReturnsThreeTwoFive() {
+    public void givenABBACCCCAC_ReturnsThreeTwoFive() {
         Score score = new Score();
         int[] abbbaccccac = score.calculateScores("ABBACCCCAC");
         assertArrayEquals(new int[]{3, 2, 5}, abbbaccccac);
