cc6a4d407624e0f6a44c37e69eadbe3f035abb5f
Vinodkumar M <vinod.kumar@improve10x.com>
2023-07-16 16:20:31 +0530

    Changes did in ScoringSystem.

---
+++ src/nthEvenNumberTest/NthEvenNumberTest.java
@@ -0,0 +1,34 @@
+package nthEvenNumberTest;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class NthEvenNumberTest {
+
+    @Test
+    public void nothing() {
+
+  }
+
+   @Test
+    public void givenTen_returnEighteen(){
+        NthEvenNumber nthEvenNumber = new NthEvenNumber();
+        int eighteen = nthEvenNumber.findNthEvenNumber(10);
+        assertEquals(18,eighteen);
+   }
+
+    @Test
+    public void givenZero_returnNegativeNumber(){
+        NthEvenNumber nthEvenNumber = new NthEvenNumber();
+        int negativeNumber = nthEvenNumber.findNthEvenNumber(0);
+        assertEquals(-1,negativeNumber);
+    }
+
+    @Test
+    public void givenNegativeNumber_returnNegativeNumber(){
+        NthEvenNumber nthEvenNumber = new NthEvenNumber();
+        int negativeNumber = nthEvenNumber.findNthEvenNumber(-10);
+        assertEquals(-1,negativeNumber);
+    }
+}
