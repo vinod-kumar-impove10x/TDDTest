44098985db24a8ba4c22a92a36c1f92aadbcff32
Vinodkumar M <vinod.kumar@improve10x.com>
2023-07-17 12:16:58 +0530

    TheFarmProblem Test completed.

---
+++ src/theFarmProblem/TheFarmProblemTest.java
@@ -0,0 +1,33 @@
+package theFarmProblem;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class TheFarmProblemTest {
+    @Test
+    public void nothing() {
+
+    }
+
+    @Test
+    public void givenNumberChic2Cows3Pigs5_returnsTotalLegs() {
+      TheFarmProblem theFarmProblem = new TheFarmProblem();
+      int TotalLegs = theFarmProblem.findNumLegs(2, 3, 5);
+        assertEquals(36,TotalLegs);
+    }
+
+    @Test
+    public void givenNumberChic1Cows2Pigs3_returnsTotalLegs() {
+        TheFarmProblem theFarmProblem = new TheFarmProblem();
+        int TotalLegs = theFarmProblem.findNumLegs(1, 2, 3);
+        assertEquals(22,TotalLegs);
+    }
+
+    @Test
+    public void givenNumberChic5Cows2Pigs8_returnsTotalLegs() {
+        TheFarmProblem theFarmProblem = new TheFarmProblem();
+        int TotalLegs = theFarmProblem.findNumLegs(5, 2, 8);
+        assertEquals(50,TotalLegs);
+    }
+}
