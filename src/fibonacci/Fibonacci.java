9a048d1cf45eb9d9b54fc0b7323e5b0715de1690
Vinodkumar M <vinod.kumar@improve10x.com>
2023-07-19 15:22:04 +0530

    Done with Refactor which is 3rd Step of TDD.

--- src/fibonacci/Fibonacci.java
+++ src/fibonacci/Fibonacci.java
@@ -2,14 +2,10 @@ package fibonacci;
 
 public class Fibonacci {
     public int findNthNumber(int n) {
-       if (n < 0){
+       if (n <= 0){
            return -1;
-       } else if ( n <= 0) {
-           return -1;
-       } else if (n == 1) {
-           return 0;
-       }else if (n == 2) {
-           return 1;
+       } else if (n == 1 ||n == 2 ) {
+           return n-1;
        } else {
            return findNthNumber(n-1) + findNthNumber(n-2);
        }
