38e2390d5883b2a2cfc20b4273a22512965fa2cd
Vinodkumar M <vinod.kumar@improve10x.com>
2023-07-25 01:29:38 +0530

    Created Temple run Package. Created Player class and playerTest. givenNameNull_whenGetNameCalled_returnEmpty test case completed.

--- src/java/com/improve10x/tdd/templerun/Player.java
+++ src/java/com/improve10x/tdd/templerun/Player.java
@@ -1,26 +1,11 @@
 package com.improve10x.tdd.templerun;
 
-import com.improve10x.tdd.templerun.Character;
+public class Player {
 
-public class Player extends Character {
-
-    private int score;
-
-    public Player(String name, int health) {
-        super(name, health);
-        this.score = 0;
+    public Player(String name) {
     }
 
-    public void run() {
-        System.out.println("Running....");
+    public String getName() {
+        return "";
     }
-
-    public void jump() {
-        System.out.println(getName() + "Jumped to avoid obstacles");
-    }
-
-    public int getScore(){
-        return score;
-    }
-
 }
---
+++ src/tests/com/improve10x/tdd/templeruntest/PlayerTest.java
@@ -0,0 +1,30 @@
+package com.improve10x.tdd.templeruntest;
+
+import com.improve10x.tdd.templerun.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlayerTest {
+    private ByteArrayOutputStream outputStream;
+    @BeforeEach
+    public void setup() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+    /*
+    Given - Providing some input
+    When - Situation or a set of actions
+    Then - Result or output
+     */
+
+    @Test
+    public void givenNameNull_whenGetNameCalled_returnEmpty(){
+        Player player = new Player(null);
+        assertEquals("",player.getName());
+    }
+}
