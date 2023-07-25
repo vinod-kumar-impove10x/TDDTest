62e8dfb02d5b7cbdf9b11fb50c330ea4c40efc69
Vinodkumar M <vinod.kumar@improve10x.com>
2023-07-25 14:57:48 +0530

    givenCoin10_whenGetScoreCalled_returns10 test case completed.

--- src/java/com/improve10x/tdd/templerun/Player.java
+++ src/java/com/improve10x/tdd/templerun/Player.java
@@ -3,9 +3,12 @@ package com.improve10x.tdd.templerun;
 public class Player {
     private String name;
     private int health;
+    private int score;
+
     public Player(String name) {
         this.name = name == null ? "" : name.trim();
         this.health = 100;
+        this.score = 0;
     }
 
     public Player(String name, int health) {
@@ -29,10 +32,11 @@ public class Player {
     }
 
     public int getScore() {
-        return 0;
+        return score;
     }
 
     public void collectCoin(Coin coin) {
+        this.score = coin.getValue();
 
     }
 
